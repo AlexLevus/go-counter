7c6a9867e094bfe21e85e9edfea86cba460694f3
Aleksandr Levus <aleksandr.l@ati.su>
2022-11-10 15:54:04 +0300

    update project structure

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,13 +3,7 @@ package app
 import (
 	"github.com/AlexLevus/go-counter/internal/repository"
 	"github.com/AlexLevus/go-counter/internal/server"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
-
-type Counter struct {
-	Value     int                `bson:"value,omitempty"`
-	UpdatedAt primitive.DateTime `bson:"updatedAt,omitempty"`
-}
 
 func Run() error {
 	counterRepository, _ := repository.NewRepository()
