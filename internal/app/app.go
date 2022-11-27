9530315ff4f2c7490681414eba7160600a903b94
Aleksandr Levus <aleksandr.l@ati.su>
2022-11-27 15:47:36 +0300

    Test deployment to Heroku

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,13 +3,14 @@ package app
 import (
 	"github.com/AlexLevus/go-counter/internal/repository"
 	"github.com/AlexLevus/go-counter/internal/server"
+	"os"
 )
 
 func Run() error {
 	counterRepository, _ := repository.NewRepository()
 	httpServer := server.NewServer(counterRepository)
 
-	err := httpServer.Run("12345")
+	err := httpServer.Run(os.Getenv("PORT"))
 
 	if err != nil {
 		return err
